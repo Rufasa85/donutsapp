8436cd3d6cb4888e86613ce4d68dbee7c8916a3e
Joe Rehfuss <joe.rehfuss@gmail.com>
2021-10-19 13:42:40 -0700

    custom middleware

--- index.js
+++ index.js
@@ -1,42 +1,18 @@
 const express = require("express");
-const path = require("path");
-const fs = require("fs");
 const app = express();
-
 const PORT = 3000;
+const htmlRoutes = require("./routes/htmlRoutes");
+const donutRoutes = require("./routes/donutRoutes");
+const logReq = require("./middleware/logRequest")
 
-const donuts = require("./db/donuts.json")
-
+app.use(logReq)
 app.use(express.static("public"))
-
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-app.get("/",(req,res)=>{
-   res.sendFile(path.join(__dirname,`public/homepage.html`))
-})
 
-app.get("/api/donuts",(req,res)=>{
-    res.json(donuts)
-})
-
-app.get("/api/donuts/:id",(req,res)=>{
-    for (let i = 0; i < donuts.length; i++) {
-        if(donuts[i].id==req.params.id){
-           return res.json(donuts[i])
-        }   
-    }
-    res.status(404).send("no donut found")
-})
-
-app.post("/api/donuts",(req,res)=>{
-    console.log(req.body);
-    donuts.push(req.body)
-    fs.writeFileSync("./db/donuts.json",JSON.stringify(donuts,null,4))
-    console.log("done")
-    res.json({message:"data recieved"})
-     
-})
+app.use(htmlRoutes)
+app.use("/api/donuts",donutRoutes)
 
 app.listen( PORT,()=>{
     console.log("listenin to port "+ PORT)
